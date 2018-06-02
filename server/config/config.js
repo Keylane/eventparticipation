1b2953a21b1985f26b0940bda9efb576cd11760a
Anders Helmar <anders.helmar@keylane.com>
2018-06-02 13:26:14 +0200

    Use db stub on production

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ function getConfig() {
 			env: 'production',
 			uri: 'mongodb://localhost:27017/eventparticipation_prod',
 			dbName: 'eventparticipation_prod',
-			dbType: 'mongodb', // change to 'mongodb' to get it to work with mongo
+			dbType: 'stub', // change to 'mongodb' to get it to work with mongo
 			port: process.env.PORT || 3000
 		};
 	} else if (envUtil.isRunningTest()) {
